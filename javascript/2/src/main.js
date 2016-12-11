8f31a412b7a6aba233b9cbb70bfeaae09503912d
ps-aux <seriouz.accz@gmail.com>
2016-12-12 00:01:26 +0100

    Add some new stuff

---
+++ javascript/2/src/main.js
@@ -0,0 +1,24 @@
+class ActionArray {
+
+    constructor() {
+        this.array = []
+        this.replayArray = []
+        this.actions = []
+    }
+
+    add(index, el) {
+        this.array[index] = el
+    }
+
+    get(index) {
+        return array[index]
+    }
+
+    swap(a, b) {
+
+    }
+
+    select(index) {
+
+    }
+}
