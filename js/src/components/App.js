1028939cb3a45e092f6a5f446b5498a1d09ce3c1
ps-aux <seriouz.accz@gmail.com>
2018-04-22 14:07:45 +0200

    Clean up file structure and old implementations

--- js/src/components/App.js
+++ js/src/components/App.js
@@ -4,39 +4,18 @@ import Tree from 'src/components/tree/Tree'
 import ArraySort from 'src/components/ArraySort'
 import {heap} from 'src/algo/data-structures/heap'
 import {range} from 'ramda'
-import Attempt from "./Attempt"
-import D3 from './D3'
 
 const sort = false
-const tree = false
+const tree = true
 
 const h = heap()
 
 range(1, 10).forEach(v => h.insert(v))
 
-let root = {
-    value: 'A',
-    left: {
-        value: 'B',
-        left: {
-            value: 'C'
-        }
-    },
-    right: {
-        value: 'D'
-    }
-}
-
-
-root = h.asTree()
-
-
 const App = () =>
     <div className="app">
-        {tree && <Tree root={root}/>}
+        {tree && <Tree/>}
         {sort && <ArraySort/>}
-        <D3/>
-        {/*<Attempt/>*/}
     </div>
 
 export default App
