94826823622480dbdc8df1f02d7d8554c67e9db3
ps-aux <seriouz.accz@gmail.com>
2018-05-10 23:09:12 +0200

    Add D3Transition hoc

--- js/src/components/tree/Tree.js
+++ js/src/components/tree/Tree.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import treeDrawer, {calcLayout} from 'src/components/tree/treeDrawer'
+import {calcLayout} from 'src/components/tree/treeDrawer'
 import './Tree.global.sass'
 import {translate} from 'src/d3/utils'
 import * as d3 from 'd3'
 import Collection from 'src/components/basic/Collection'
 import {withProps} from 'recompose'
-import {prop} from 'ramda'
-import Animate from 'react-move/Animate'
-import {easeExpOut} from 'd3'
+import {equals, identity, prop} from 'ramda'
 
 
 const Node = ({value, r = 10, x, y, _ref}) =>
@@ -21,31 +19,74 @@ const link = d3.linkVertical()
     .x(prop('x'))
     .y(prop('y'))
 
-const Link = props =>
-    <path className="link" d={link(props)}/>
+const Link = ({d, _ref}) =>
+    <path ref={_ref} className="link" d={d}/>
 
 
-const TNode = props =>
-    <Animate
-        start={() => props}
-        update={() => ({
-            x: [props.x],
-            timing: {duration: 750, ease: easeExpOut}
-        })}>
-        {s => <Node {...s} />}
-    </Animate>
+const withTransition = ({
+                            transition,
+                            hasChanged = equals,
+                            attrs = identity
+                        }) => Comp => {
 
-const TLink = props =>
-    <Animate
-        start={() => props}
-        update={() => ({
-            source: [{x: props.source.x, y: props.source.y}],
-            target: [{x: props.target.x, y: props.target.y}],
-            timing: {duration: 750, ease: easeExpOut}
-        })}>
-        {s => <Link {...s} />}
-    </Animate>
+    class D3Transition extends React.Component {
+        constructor(props) {
+            super(props)
+            this.state = {...props}
+        }
 
+        ref = el => this.el = el
+
+
+        componentDidUpdate() {
+            const {state, props} = this
+            if (hasChanged(state, props)) {
+                d3.select(this.el)
+                    .transition(transition)
+                    .attrs(attrs(props))
+                    .on('end', () => this.setState({...this.props}))
+            }
+        }
+
+        render = () => <Comp {...this.state}
+                             _ref={this.ref}/>
+    }
+
+    return D3Transition
+
+}
+
+const hasChanged = (a, b) => {
+    if (!a && !b)
+        return false
+    if (!a || !b)
+        return true
+
+    return a.x !== b.x || a.y !== b.y
+}
+
+
+const transition = d3.transition()
+    .duration(300)
+    .ease(d3.easeBounce)
+
+
+
+const hasLinkChanged = ({source: s1, target: t1},
+                        {source: s2, target: t2}) => hasChanged(s1, s2) || hasChanged(t1, t2)
+
+
+
+TNode = withTransition({
+    transition,
+    hasChanged,
+    attrs: props => ({transform: translate(props)})
+})(Node)
+
+TLink = withTransition({
+    transition,
+    hasChanged: (a, b) => a.d !== b.d
+})(Link)
 
 
 const Tree = ({layout}) =>
@@ -53,7 +94,10 @@ const Tree = ({layout}) =>
         <svg width="1200" height="800">
             <g className="tree" transform={translate({x: 400, y: 20})}>
                 <g className="links">
-                    <Collection items={layout.links}
+                    <Collection items={layout.links.map(l => ({
+                        ...l,
+                        d: link(l)
+                    }))}
                                 id={({source, target}) => `${source.id}-${target.id}`}
                                 comp={TLink}/>
                 </g>
