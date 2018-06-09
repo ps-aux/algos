b2773bca98e31fe919b6c3bbbf72d8e3d1b5bd9d
ps-aux <seriouz.accz@gmail.com>
2018-06-09 20:16:35 +0200

    Add mergeList alg

---
+++ js/src/algo/other/mergeLists.js
@@ -0,0 +1,29 @@
+export const mergeLists = (a, b) => {
+
+    const pickSmaller = () => {
+        if (!a.length && b.length)
+            throw new Error('Both lists are empty')
+
+        if (!a.length)
+            return b.shift()
+
+        if (!b.length)
+            return a.shift()
+
+        const aEl = a[0]
+        const bEl = b[0]
+
+        if (aEl < bEl)
+            return a.shift()
+        else
+            return b.shift()
+    }
+
+    const finalSize = a.length + b.length
+    const res = []
+
+    while(res.length < finalSize)
+        res.push(pickSmaller())
+
+    return res
+}
