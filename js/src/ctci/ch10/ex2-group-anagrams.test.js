3f42ee770a016a27026785bcdde176e1feab9f4b
psaux <seriouz.accz@gmail.com>
2018-12-03 22:14:41 +0100

    wip

--- js/src/ctci/ch10/ex2-group-anagrams.test.js
+++ js/src/ctci/ch10/ex2-group-anagrams.test.js
@@ -2,39 +2,52 @@ const alphabetStart = 97
 const alphabetEnd = 122
 
 const anagaramId = w => {
-  const counts = []
+    const counts = []
 
-  for (let i = 0; i < w.length; i++) {
-    const c = w.charCodeAt(i)
-    const count = counts[c] || 0
-    counts[c] = count + 1
-  }
+    for (let i = 0; i < w.length; i++) {
+        const c = w.charCodeAt(i)
+        const count = counts[c] || 0
+        counts[c] = count + 1
+    }
 
-  const res = []
-  for (let i = alphabetStart; i < +alphabetEnd; i++) {
-    const count = counts[i]
+    const res = []
+    for (let i = alphabetStart; i < +alphabetEnd; i++) {
+        const count = counts[i]
 
-    if (count)
-      res.push(`${i}=${count}`)
-  }
+        for (let j = 0; j < count; j++)
+            res.push(String.fromCharCode(i))
 
-  return res.join(',')
+    }
+
+    return res.join(',')
 }
 
 it('anagramId', () => {
 
-  expect(anagaramId('aab')).toBe(anagaramId('baa'))
-  expect(anagaramId('c')).toBe(anagaramId('c'))
-  expect(anagaramId('abc')).not.toBe(anagaramId('abe'))
+    expect(anagaramId('aab')).toBe(anagaramId('baa'))
+    expect(anagaramId('c')).toBe(anagaramId('c'))
+    expect(anagaramId('abc')).not.toBe(anagaramId('abe'))
 
 })
 
 const groupAnagrams = arr => {
 
-  return arr
-  .map(w => [w, anagaramId(w)])
-  .sort((a, b) => a[1] < b[1])
-  .map(p => p[0])
+    const map = {}
+
+    // Put to buckets
+    arr.forEach(w => {
+        const id = anagaramId(w)
+        const bucket = map[id] || (map[id] = [])
+        bucket.push(id)
+    })
+
+    const res = []
+    // flatten
+    Object.values(map).forEach(b =>
+        b.forEach(
+            w => res.push(w)))
+
+    return res
 
 }
 
@@ -43,13 +56,9 @@ const test = (arr) => {
 }
 
 describe('test', () => {
-  it('case 1', () => {
-    const res = groupAnagrams(['aba', 'c', 'hu', 'baa', 'uh', 'aab'])
-    // expect(A).toMatchObject('')
-
-    console.log(res)
-
-  })
-
+    it('case 1', () => {
+        const res = groupAnagrams(['aba', 'c', 'hu', 'baa', 'uh', 'aab'])
+        console.log(res)
+    })
 })
 
---
+++ js/src/ctci/ch10/ex3-search-in-rotated-array.test.js
@@ -0,0 +1,62 @@
+const isInArray = (arr, val, start, end) => {
+    const first = arr[start]
+    const last = arr[end - 1]
+
+    if (first <= last)  // normally sorted
+        return first <= val && val <= last
+    else
+    // else contains inflection point
+        return val >= first || val <= last
+}
+
+const search = (arr, val, start = 0, end = arr.length) => {
+    console.log(start, end)
+    if (start === end)
+        if (arr[start] === val)
+            return start
+        else
+            return -1
+
+    const mid = Math.floor((start + end) / 2)
+
+    if (isInArray(arr, val, start, mid))
+        return search(arr, val, start, mid)
+
+    if (!isInArray(arr, val, mid, end))
+        return -1
+
+    return search(arr, val, mid, end)
+}
+
+describe('isInArray', () => {
+
+    const test = (arr, val, yes = true) =>
+        expect(isInArray(arr, 0, arr.length, val)).toBe(yes)
+
+    it('case 1', () => {
+        const arr = [1, 2, 3]
+        test(arr, 2)
+        test(arr, 1)
+        test(arr, 3)
+        test(arr, 4, false)
+    })
+
+    it('case 2', () => {
+        const arr = [4, 5, 1]
+        test(arr, 4)
+        test(arr, 5)
+        test(arr, 1)
+        test(arr, 2, false)
+    })
+
+})
+
+describe('test', () => {
+
+    it('case 1', () => {
+        const res = search([15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14], 5)
+
+        expect(res).toBe(8)
+    })
+})
+
