518571e98645870b2598f43867ce691c674d1ed8
psaux <seriouz.accz@gmail.com>
2019-03-13 21:15:33 +0100

    wip

---
+++ js/src/ctci/ch10/ex9-sorted-matrix-search.test.ts
@@ -0,0 +1,20 @@
+const search = (mtx, val) => {
+}
+
+describe('test', () => {
+
+    const mat = [
+        [1, 5, 8],
+        [6, 7, 10],
+        [7, 8, 11]
+    ]
+
+    it('case 1',
+        test('ball', arr, 6))
+    it('case 2',
+        test('at', arr, 2))
+    it('case 3',
+        test('not', arr, -1))
+
+})
+
