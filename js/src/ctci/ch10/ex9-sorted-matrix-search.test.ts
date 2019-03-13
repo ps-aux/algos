9b81dc3a859cac4e65a20236091fe7333e1054fe
psaux <seriouz.accz@gmail.com>
2019-03-13 21:15:39 +0100

    wip

--- js/src/ctci/ch10/ex9-sorted-matrix-search.test.ts
+++ js/src/ctci/ch10/ex9-sorted-matrix-search.test.ts
@@ -1,20 +1,59 @@
-const search = (mtx, val) => {
+const search = (
+    mtx: number[][],
+    y: number, x: number,
+    val: number): number[] => {
+
+    const mSize = mtx.length
+    const nSize = mtx[0].length
+
+    const xyVal = mtx[y] [x]
+    if (xyVal === val)
+        return [y, x]
+
+    if (xyVal > val)
+        return null
+
+    if (xyVal)
+
+        if (y === mSize)
+            return null
+    if (x === nSize)
+        return null
+
+    const inColDir = search(mtx, y, x + 1, val)
+
+    if (inColDir)
+        return inColDir
+
+    y = y + 1
+    while (y < mSize) {
+        const v = mtx[y][x]
+        if (v === val)
+            return [y, x]
+        if (v > val)
+            break
+        y++
+    }
+
+    return null
 }
 
-describe('test', () => {
+it('test', () => {
 
-    const mat = [
-        [1, 5, 8],
-        [6, 7, 10],
-        [7, 8, 11]
+    const mtx = [
+        [1, 5, 8, 12],
+        [6, 7, 10, 13],
+        [7, 8, 11, 14]
     ]
 
-    it('case 1',
-        test('ball', arr, 6))
-    it('case 2',
-        test('at', arr, 2))
-    it('case 3',
-        test('not', arr, -1))
+    const r = search(mtx, 0, 0, 5)
+    console.log(r)
+
+    expect(search(mtx, 0, 0, 5)).toEqual([0, 1])
+    expect(search(mtx, 0, 0, 7)).toEqual([1, 1])
+    expect(search(mtx, 0, 0, 1)).toEqual([0, 0])
+    expect(search(mtx, 0, 0, 6)).toEqual([1, 0])
+    expect(search(mtx, 0, 0, 14)).toEqual([2, 3])
 
 })
 
