d7f12991b6511be7a373a500fb228813e186bcb6
psaux <seriouz.accz@gmail.com>
2019-03-14 21:05:02 +0100

    Improve ex10 with counting BST

--- js/src/ctci/ch10/ex10-rank-from-stream.test.ts
+++ js/src/ctci/ch10/ex10-rank-from-stream.test.ts
@@ -1,86 +1,111 @@
-const x = () =>
-    123
-
-const Processor = () => {
-    const arr: number[] = []
-
-    return {
-        track: (n: number) => {
-            arr.push(n)
-            arr.sort((a, b) => a - b)
-        },
-        getRankOfNumber: (n: number) => {
-            for (let i = 0; i < arr.length; i++) {
-                if (arr[i] > n) return i - 1
-            }
-            return -1
-        }
-    }
-}
-
-it('test', () => {
-    const p = Processor()
-    ;[5, 1, 4, 4, 5, 9, 7, 13, 3].forEach(p.track)
-
-    expect(p.getRankOfNumber(1)).toBe(0)
-    expect(p.getRankOfNumber(3)).toBe(1)
-    expect(p.getRankOfNumber(4)).toBe(3)
-})
+const x = () => 123
 
 type TreeNode = {
-    val: number,
-    count: number,
-    children: TreeNode[]
+  val: number
+  count: number
+  children: TreeNode[]
 }
 
 const createNode = (val: number): TreeNode => ({
-    val,
-    count: 1,
-    children: []
+  val,
+  count: 1,
+  children: []
 })
 
 const add = (node: TreeNode, val: number) => {
-    if (node.val === val) {
-        node.count++
-    } else if (val < node.val) {
-        const left = node.children[0]
-        if (!left)
-            node.children[0] = createNode(val)
-        else
-            add(left, val)
-    } else if (val > node.val) {
-        const right = node.children[1]
-        if (!right)
-            node.children[1] = createNode(val)
-        else
-            add(right, val)
-    }
-}
-
-const toArray = (node: TreeNode, acc: number[] = []) => {
+  if (node.val === val) {
+    node.count++
+  } else if (val < node.val) {
     const left = node.children[0]
+    if (!left) node.children[0] = createNode(val)
+    else add(left, val)
+  } else if (val > node.val) {
     const right = node.children[1]
-
-    if (left)
-        toArray(left, acc)
-    acc.push(node.val)
-    if (right)
-        toArray(right, acc)
-
-    return acc
+    if (!right) node.children[1] = createNode(val)
+    else add(right, val)
+  }
 }
 
-it('bst', () => {
-    const t = createNode(2)
-    add(t, 4)
-    add(t, 5)
-    add(t, 100)
-    add(t, 58)
-    add(t, 288)
-    add(t, 1)
+const nodesUpTo = (
+  node: TreeNode, val: number, acc: TreeNode[] = []): TreeNode[] | null => {
 
-    const a = toArray(t)
+  const left = node.children[0]
+  const right = node.children[1]
 
-    console.log('arr', a)
+  if (left) {
+    const res = nodesUpTo(left, val, acc)
+    if (res)
+      return res
+  }
 
+  acc.push(node)
+  // We hit the end
+  if (node.val === val)
+    return acc
+
+  if (right)
+    return nodesUpTo(right, val, acc)
+
+  return null
+}
+
+const Ranker = (rootVal: number) => {
+  const t = createNode(rootVal)
+  return {
+    add: (v: number) => add(t, v),
+    rankOf: (v: number) => {
+      const res = nodesUpTo(t, v)
+      if (!res)
+        throw new Error(`Value ${v} not found`)
+
+      // Exclude the 'index' of the matching value
+      return res.reduce((acc, v) => acc + v.count, 0) - 1
+    }
+  }
+}
+
+it('Ranker', () => {
+
+  const tree = Ranker(2)
+
+  expect(tree.rankOf(2)).toBe(0)
+
+  tree.add(1)
+  expect(tree.rankOf(2)).toBe(1)
+
+  tree.add(1)
+  expect(tree.rankOf(2)).toBe(2)
+
+  tree.add(10)
+  expect(tree.rankOf(2)).toBe(2)
+
+  tree.add(3)
+  expect(tree.rankOf(10)).toBe(4)
+
+  expect(tree.rankOf(1)).toBe(0)
 })
+
+const Processor = () => {
+  let ranker: any
+
+  return {
+    track: (n: number) => {
+      if (!ranker)
+        ranker = Ranker(n)
+      else
+        ranker.add(n)
+    },
+    getRankOfNumber: (n: number) => ranker.rankOf(n)
+  }
+}
+
+it('test', () => {
+  const p = Processor()
+  ;[5, 1, 4, 4, 5, 9, 7, 13, 3].forEach(p.track)
+
+  expect(p.getRankOfNumber(1)).toBe(0)
+  expect(p.getRankOfNumber(3)).toBe(1)
+  expect(p.getRankOfNumber(4)).toBe(3)
+  expect(p.getRankOfNumber(7)).toBe(6)
+})
+
